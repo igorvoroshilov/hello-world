626d272be3933230b16842abbf82daa214e936cf
Sergey Voroshilov <voroshilov.grk@yandex.ru>
2017-03-26 01:21:47 +0300

    fix bugs

--- hunger.js
+++ hunger.js
@@ -1,6 +1,7 @@
 var y = 2 + 2;
 console.log(y);
-if y == 4 then 
-    console.log('Нифига себе') 
-else 
+if (y == 4) { 
+    console.log('Нифига себе'); 
+} else { 
     console.log('Ну блин, пойду поем');
+}
